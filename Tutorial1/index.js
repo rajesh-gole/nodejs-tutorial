ed24eb36b3a69d36297686a320c2703c0284fdcf
RAJESH GOLE <rajesh.truematrix@gmail.com>
2024-04-15 18:25:44 +0530

    update

---
+++ Tutorial1/index.js
@@ -0,0 +1,11 @@
+const {getName, getAge, gender} = require('./student')
+
+
+
+console.log(getName());
+console.log(getAge());
+console.log(gender);
+
+// console.log(s.getName());
+// console.log(s.getAge());
+// console.log(s.gender);
---
+++ Tutorial1/student.js
@@ -0,0 +1,18 @@
+const getName = () => {
+    return 'Rajesh Gole';
+}
+
+const getAge = () => {
+    return '26';
+}
+
+const gender = 'Male';
+
+// exports.Name = getName;
+// exports.getAge = getAge;
+// exports.gender = gender;
+
+module.exports = {
+    getName, getAge, gender
+}
+
